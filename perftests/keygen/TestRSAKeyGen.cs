486be01f15ce06c92d0a94138ffafc7e8e7d5f82
Reesli <tobiasrees@gmail.com>
2015-08-11 16:03:32 +0200

    Add Performance Test for Key Generation SHA3/RSA

---
+++ perftests/keygen/TestRSAKeyGen.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CryptobySharp
+{
+	public class TestRSAKeyGen
+	{
+		const int cycles = 1;
+		int keySize;
+		byte[] privKey;
+		byte[] pubKey;
+
+		KeyGenRSA keyGen;
+
+		[TestFixtureSetUp] public void Init()
+		{ 
+			CryptobyClient client = new CryptobyClient();
+			CryptobyCore core = new CryptobyCore(client);
+			keyGen = new KeyGenRSA (core);
+		}
+
+		public void testGenRSAKey() {
+			keyGen.initGenerator (keySize);
+		}
+
+		[Test]
+		public void testRun(){
+			const int rounds = 1;
+
+			Console.WriteLine ("Test Performance RSA Key Generation");
+			Console.WriteLine ("Rounds: " + rounds);
+			Console.WriteLine ("Cycles per Round: " + cycles);
+
+			keySize = 1024;
+			Console.WriteLine ("\nGenerate RSA Key 1024 Bit");
+			PerfMeter.run(new Action(testGenRSAKey),rounds);
+			privKey = keyGen.getPrivateKeyByte();
+			pubKey = keyGen.getPublicKeyByte();
+			Assert.AreEqual(privKey.Length*4,keySize );
+			Assert.AreEqual(pubKey.Length*8,keySize );
+
+			keySize = 2048;
+			Console.WriteLine ("\nGenerate RSA Key 2048 Bit");
+			PerfMeter.run(new Action(testGenRSAKey),rounds);
+			privKey = keyGen.getPrivateKeyByte();
+			pubKey = keyGen.getPublicKeyByte();
+			Assert.AreEqual(privKey.Length*4,keySize );
+			Assert.AreEqual(pubKey.Length*8,keySize );
+
+			keySize = 4096;
+			Console.WriteLine ("\nGenerate RSA Key 4096 Bit");
+			PerfMeter.run(new Action(testGenRSAKey),rounds);
+			privKey = keyGen.getPrivateKeyByte();
+			pubKey = keyGen.getPublicKeyByte();
+			Assert.AreEqual(privKey.Length*4,keySize );
+			Assert.AreEqual(pubKey.Length*8,keySize );
+
+		}
+	}
+}
+	
---
+++ perftests/keygen/TestSHA3KeyGen.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CryptobySharp
+{
+	public class TestSHA3KeyGen
+	{
+		const int cycles = 10000;
+		int keySize;
+		byte[] privKey;
+
+		KeyGenSHA3 keyGen;
+
+		[TestFixtureSetUp] public void Init()
+		{ 
+			keyGen = new KeyGenSHA3 ();
+		}
+
+		public void testGenSHA3Key() {
+			for(int i = 0; i < cycles; i++){
+				privKey = keyGen.generateKeyByte (keySize,"password");
+			}
+		}
+
+		[Test]
+		public void testRun(){
+			const int rounds = 1;
+
+			Console.WriteLine ("Test Performance SHA3 Key Generation");
+			Console.WriteLine ("Rounds: " + rounds);
+			Console.WriteLine ("Cycles per Round: " + cycles);
+
+			keySize = 224;
+			Console.WriteLine ("\nGenerate SHA3 Key 224 Bit");
+			PerfMeter.run(new Action(testGenSHA3Key),rounds);
+			Assert.AreEqual(privKey.Length*8,keySize );
+
+			keySize = 256;
+			Console.WriteLine ("\nGenerate SHA3 Key 256 Bit");
+			PerfMeter.run(new Action(testGenSHA3Key),rounds);
+			Assert.AreEqual(privKey.Length*8,keySize );
+
+			keySize = 384;
+			Console.WriteLine ("\nGenerate SHA3 Key 384 Bit");
+			PerfMeter.run(new Action(testGenSHA3Key),rounds);
+			Assert.AreEqual(privKey.Length*8,keySize );
+
+			keySize = 512;
+			Console.WriteLine ("\nGenerate SHA3 Key 512 Bit");
+			PerfMeter.run(new Action(testGenSHA3Key),rounds);
+			Assert.AreEqual(privKey.Length*8,keySize );
+		}
+	}
+}
+	
